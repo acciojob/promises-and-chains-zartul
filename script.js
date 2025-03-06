48cc38fb35d9cd086c425f48856980c6ff998950
zartul <121471819+zartul@users.noreply.github.com>
2025-03-06 11:32:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+document.getElementById("voteForm").addEventListener("submit", function(event) {
+            event.preventDefault();
+            
+            let userName = document.getElementById("name").value.trim();
+            let userAge = document.getElementById("age").value.trim();
+            
+            if (userName === "" || userAge === "") {
+                alert("Please enter valid details.");
+                return;
+            }
+            
+            userAge = Number(userAge);
+            
+            let checkVotingEligibility = new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    if (userAge > 18) {
+                        resolve(`Welcome, ${userName}. You can vote.`);
+                    } else {
+                        reject(`Oh sorry ${userName}. You aren't old enough.`);
+                    }
+                }, 4000);
+            });
+            
+            checkVotingEligibility
+                .then(message => alert(message))
+                .catch(error => alert(error));
+        });
